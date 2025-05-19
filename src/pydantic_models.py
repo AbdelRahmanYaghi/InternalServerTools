da4c750bfde913398295bf7bd14949d383245e71
AbdelrahmanYaghi <ayaghi@avertra.com>
2025-05-19 13:39:07 +0300

    Added default text embedding model

--- src/pydantic_models.py
+++ src/pydantic_models.py
@@ -3,6 +3,6 @@ from typing import Optional, Any
 
 class JSONTextEmbeddingRequest(BaseModel):
     text: list[str]
-    embedding_model: str
+    embedding_model: str = "sentence-transformers/all-MiniLM-L6-v2"
     model_kwargs: Optional[dict] = {} 
     
